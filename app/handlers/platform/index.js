ec311934b39f32da6e606e9be5548b43e3660a9a
Tyler Cvetan <tyler.cvetan@gmail.com>
2015-10-17 15:12:37 -0500

    Integrating react-redux to get access to store information
    Signed-off-by: Tyler Cvetan <tyler.cvetan@gmail.com>

--- app/handlers/platform/index.js
+++ app/handlers/platform/index.js
@@ -1,8 +1,14 @@
 import React, {Component} from 'react';
+import {connect} from 'react-redux';
 import PlatformForm from '../../components/platform/PlatformForm';
 
 // not sure what this will all do yet but
 class PlatformHandler extends Component {
+  constructor(props) {
+    super(props);
+    console.log(props);
+  }
+
   render() {
     return (
       <div>
@@ -14,4 +20,4 @@ class PlatformHandler extends Component {
   }
 }
 
-export default PlatformHandler;
+export default connect()(PlatformHandler);
