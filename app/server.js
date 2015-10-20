c502a240a620ba6f0cdbb72ec4d5d7e0d42830bc
Tyler Cvetan <tyler.cvetan@gmail.com>
2015-10-20 18:48:24 -0500

    Setting up temporary proxy server to handle api requests
    Signed-off-by: Tyler Cvetan <tyler.cvetan@gmail.com>

--- app/server.js
+++ app/server.js
@@ -11,6 +11,7 @@ import routes from './routes';
 import write from './utils/write';
 import { Provider } from 'react-redux';
 import fetchComponentData from './utils/fetchComponentData';
+import httpProxy from 'http-proxy';
 
 const history = createMemoryHistory();
 
@@ -20,45 +21,61 @@ const styles = fs.readFileSync(__dirname + '/../public/styles/styles.css');
 const htmlRegex = /¡HTML!/;
 const dataRegex = /¡DATA!/;
 
+// setup temporary proxy server
+const proxy = httpProxy.createProxyServer();
+
+
 const app = http.createServer((req, res) => {
   const cookies = new Cookies(req, res);
   const token = cookies.get('token') || uuid();
   cookies.set('token', token, { maxAge: 30 * 24 * 60 * 60 });
-  switch (req.url) {
-  case '/js/main.js':
-    return write(mainJS, 'text/javascript', res);
-  case '/favicon.ico':
-    return write('haha', 'text/plain', res);
-  case '/styles.css':
-    return write(styles, 'text/css', res);
-  default:
-    const location = history.createLocation(req.url);
-    const store = configureStore();
 
-    match({routes, location}, (error, redirectLocation, renderProps) => {
-      // I think at this point we could add custom data to the renderProps to pass
-      // to our components
-      if (redirectLocation) {
-        res.redirect(301, redirectLocation.pathname + redirectLocation.search);
-      } else if (error) {
-        res.send(500, error.message);
-      } else if (renderProps === null) {
-        res.writeHead(404, { 'Content-Type': 'text/html' });
-        res.end();
-      } else {
-        console.log(renderProps.components);
-        fetchComponentData(store.dispatch, renderProps.components, renderProps.params).then(() => {
-          const initialData = store.getState();
-          const html = renderToString(<Provider store={store}><RoutingContext {...renderProps}/></Provider>);
-          const output = indexHTML.
-            replace(htmlRegex, html).
-            replace(dataRegex, JSON.stringify(initialData));
-          write(output, 'text/html', res);
-        });
-      }
-    });
+  // something like this will probably stay
+  if (req.url.startsWith('/rest')) {
+    proxy.web(req, res, { target: 'http://localhost:9000' });
+  } else {
+    switch (req.url) {
+    case '/js/main.js':
+      return write(mainJS, 'text/javascript', res);
+    case '/favicon.ico':
+      return write('haha', 'text/plain', res);
+    case '/styles.css':
+      return write(styles, 'text/css', res);
+    default:
+      const location = history.createLocation(req.url);
+      const store = configureStore();
+
+      match({routes, location}, (error, redirectLocation, renderProps) => {
+        // I think at this point we could add custom data to the renderProps to pass
+        // to our components
+        if (redirectLocation) {
+          res.redirect(301, redirectLocation.pathname + redirectLocation.search);
+        } else if (error) {
+          res.send(500, error.message);
+        } else if (renderProps === null) {
+          res.writeHead(404, { 'Content-Type': 'text/html' });
+          res.end();
+        } else {
+          console.log(renderProps.components);
+          fetchComponentData(store.dispatch, renderProps.components, renderProps.params).then(() => {
+            const initialData = store.getState();
+            const html = renderToString(<Provider store={store}><RoutingContext {...renderProps}/></Provider>);
+            const output = indexHTML.
+              replace(htmlRegex, html).
+              replace(dataRegex, JSON.stringify(initialData));
+            write(output, 'text/html', res);
+          });
+        }
+      });
+    }
   }
 });
 
 app.listen(process.env.PORT || 5000);
 
+// this is only temporary, we will create a true api server later
+const temp = http.createServer((req, res) => {
+  write(JSON.stringify({}), 'application/json', res);
+});
+
+temp.listen(9000);
