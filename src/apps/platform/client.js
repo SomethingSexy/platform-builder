36bc07dc0e87a05dae67724dce41520c3f1037d0
Tyler Cvetan <tyler.cvetan@gmail.com>
2016-03-22 13:33:09 -0500

    AND switched over to webpack so I don't need to maintain two sets of dependencies
    Signed-off-by: Tyler Cvetan <tyler.cvetan@gmail.com>

--- src/apps/platform/client.js
+++ src/apps/platform/client.js
@@ -1,6 +1,6 @@
 // Not sure how to handle CSS yet but I can't included via component files because it fails to load on the server-side
-import 'bootstrap/css/bootstrap.css!';
-import '../../common/components/parts/parts.css!';
+import 'bootstrap/dist/css/bootstrap.css';
+import '../../common/components/parts/parts.css';
 import React from 'react';
 import { Router, browserHistory } from 'react-router';
 import routes from './routes.js';
